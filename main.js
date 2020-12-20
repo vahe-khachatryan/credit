71143a9cb30c9711c364669c52b44c67049931ff
vahe-khachatryan <vahe.khachatryann@gmail.com>
2020-12-20 22:34:30 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+document.getElementById('credit').addEventListener("change", function () {
+    document.getElementById('money').selectedIndex = document.getElementById('period').selectedIndex = document.getElementById('credit').selectedIndex;
+}, false);
+
